51efe39ee4806e272b4029f28f51ad0ed49fb24f
Marcio <fannekls@gmail.com>
2021-06-20 21:39:41 -0300

    hasta aca funciona validacion DNI

---
+++ ProyectoBanco/DniException.cs
@@ -0,0 +1,27 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: Marcio
+ * Fecha: 20/6/2021
+ * Hora: 20:50
+ * 
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+using System;
+
+namespace ProyectoBanco
+{
+	/// <summary>
+	/// Description of DniException.
+	/// </summary>
+	public class DniException : Exception 
+	{
+		public string mensaje;
+		
+		public DniException(string mensaje){
+			
+			this.mensaje=mensaje;
+		}
+		
+		
+	}
+}
--- ProyectoBanco/Program.cs
+++ ProyectoBanco/Program.cs
@@ -13,20 +13,20 @@ namespace ProyectoBanco
 			bool flag=true;
 			
 			while(flag){
-					
-					Console.WriteLine("Gestión {0}",galicia.NombreBanco);
-					Console.WriteLine("________________________________\n");
-					Console.WriteLine("Elija su operacion:\n"+
-					"\na) Agregar cuenta bancaria" +
-					"\nb) Eliminar cuenta bancaira" +
-					"\nc) Listado de clientes que tienen mas de una cuenta"+
-					"\nd) Realizar extraccion"+
-					"\ne) Depositar dinero de cuenta"+
-					"\nf) Listado de cuentas bancarias"+
-					"\ng) Listado de clientes"+
-					"\nh) Finalizar programa"+
-					"\n");
-					string menu = Console.ReadLine();
+				
+				Console.WriteLine("Gestión {0}",galicia.NombreBanco);
+				Console.WriteLine("________________________________\n");
+				Console.WriteLine("Elija su operacion:\n"+
+				                  "\na) Agregar cuenta bancaria" +
+				                  "\nb) Eliminar cuenta bancaira" +
+				                  "\nc) Listado de clientes que tienen mas de una cuenta"+
+				                  "\nd) Realizar extraccion"+
+				                  "\ne) Depositar dinero de cuenta"+
+				                  "\nf) Listado de cuentas bancarias"+
+				                  "\ng) Listado de clientes"+
+				                  "\nh) Finalizar programa"+
+				                  "\n");
+				string menu = Console.ReadLine();
 				if(menu=="a" || menu=="A"){
 					
 					OpcionA(galicia);
@@ -53,7 +53,7 @@ namespace ProyectoBanco
 			
 			
 		}
-	}
+		
 		//OPCIONES MODULARIZADAS
 		
 		public static void OpcionA (Banco banco){
@@ -62,92 +62,113 @@ namespace ProyectoBanco
 			int dniTitular=0;
 			
 			bool otraCuenta=true;
+			bool dniInvalidoValido=true;
+
 			
 			while(otraCuenta){
 				
-			Console.WriteLine("Ingrese dni del cliente");
-			try{
-			
-				 dniTitular= int.Parse(Console.ReadLine());
-			}
-			catch(FormatException ex){
-				
-				Console.WriteLine("Error. Ingrese un valor numerico.");
-				
-			}
-			
-			if(!banco.esCliente(dniTitular)){
-				
-				
-				Console.WriteLine("Nuevo cliente, ingrese el nombre: ");
-				string nombreNuevo= Console.ReadLine();
-				
-				
-				Console.WriteLine("Ingrese apellido: ");
-				string apellidoNuevo = Console.ReadLine();
-			
-				
-				Console.WriteLine("Ingrese dirección: ");
-				string direccioNueva= Console.ReadLine();
-				
-				Console.WriteLine("Ingrese telefono: ");
-				int telefonoNuevo=int.Parse(Console.ReadLine());
-				
-				Console.WriteLine("Ingrese E-mail: ");
-				string emailNuevo=Console.ReadLine();
-				
-				banco.AgregarCliente(nombreNuevo,apellidoNuevo,dniTitular,direccioNueva,telefonoNuevo,emailNuevo);
-				
-				Console.WriteLine("Ingrese numero de cuenta: ");
-				int numeroCuentaNueva = int.Parse(Console.ReadLine());
-			
-			
-				Console.WriteLine("Ingrese monto inicial");
-				double montoInicial= double.Parse(Console.ReadLine());
-				
-				banco.AltaCuenta(numeroCuentaNueva,apellidoNuevo,dniTitular,montoInicial);
-				
-				
-				Console.WriteLine("El cliente y su cuenta fueron guardados exitosamente");
-				
-				
-				
-			}
-			else{
-				
-				Console.WriteLine("Ingrese numero de cuenta: ");
-				int numeroCuenta = int.Parse(Console.ReadLine());
-			
-				Console.WriteLine("Ingrese apellido: ");
-				string apellidoNuevo = Console.ReadLine();
-			
-				Console.WriteLine("Ingrese monto inicial");
-				double montoInicial= double.Parse(Console.ReadLine());
-				
-				banco.AltaCuenta(numeroCuenta,apellidoNuevo,dniTitular,montoInicial);
-				
-				
-				Console.WriteLine("Cuenta bancaria creada exitosamente");
-				
-			}
-			
-			Console.WriteLine("Desea realizar cargar otra cuenta? " +
-			                  "\n <ENTER> para seguir, <N> para salir");
-			
-			string otraOperacion = Console.ReadLine();
-			if(otraOperacion.ToUpper()=="N"){
-				
-				otraCuenta=false;
-				
+				while(dniInvalidoValido){
+					
+					
+					Console.WriteLine("Ingrese dni del cliente");
+					
+					try{
+						
+						dniTitular= int.Parse(Console.ReadLine());
+						
+						if(dniTitular.ToString().ToCharArray().Length != 8){
+							
+							throw new DniException("se rompio todillo");
+						}
+						
+						Console.WriteLine("PASE POR ACA");
+						dniInvalidoValido=false;
+						
+					} catch(FormatException ex){
+						
+						Console.WriteLine("Error. Ingrese un valor numerico.");
+						
+						dniTitular=1;
+						
+					} catch(DniException ex){
+						
+						Console.WriteLine("DNI invalido wachin");
+						
+						dniTitular=1;
+						
+					} catch(Exception ex){
+						
+						Console.WriteLine("INTERNAL ERROR");
+						
+						dniTitular=1;
+						
+					}
 				}
-			
+				
+				
+				if(!banco.esCliente(dniTitular)){
+					
+					
+					Console.WriteLine("Nuevo cliente, ingrese el nombre: ");
+					string nombreNuevo= Console.ReadLine();
+					
+					Console.WriteLine("Ingrese apellido: ");
+					string apellidoNuevo = Console.ReadLine();
+					
+					Console.WriteLine("Ingrese dirección: ");
+					string direccioNueva= Console.ReadLine();
+					
+					Console.WriteLine("Ingrese telefono: ");
+					int telefonoNuevo=int.Parse(Console.ReadLine());
+					
+					Console.WriteLine("Ingrese E-mail: ");
+					string emailNuevo=Console.ReadLine();
+					
+					banco.AgregarCliente(nombreNuevo,apellidoNuevo,dniTitular,direccioNueva,telefonoNuevo,emailNuevo);
+					
+					Console.WriteLine("Ingrese numero de cuenta: ");
+					int numeroCuentaNueva = int.Parse(Console.ReadLine());
+					
+					
+					Console.WriteLine("Ingrese monto inicial");
+					double montoInicial= double.Parse(Console.ReadLine());
+					
+					banco.AltaCuenta(numeroCuentaNueva,apellidoNuevo,dniTitular,montoInicial);
+					
+					
+					Console.WriteLine("El cliente y su cuenta fueron guardados exitosamente");
+					
+				}
+				else{
+					
+					Console.WriteLine("Ingrese numero de cuenta: ");
+					int numeroCuenta = int.Parse(Console.ReadLine());
+					
+					Console.WriteLine("Ingrese apellido: ");
+					string apellidoNuevo = Console.ReadLine();
+					
+					Console.WriteLine("Ingrese monto inicial");
+					double montoInicial= double.Parse(Console.ReadLine());
+					
+					banco.AltaCuenta(numeroCuenta,apellidoNuevo,dniTitular,montoInicial);
+					
+					
+					Console.WriteLine("Cuenta bancaria creada exitosamente");
+					
+				}
+				
+				Console.WriteLine("Desea realizar cargar otra cuenta? " +
+				                  "\n <ENTER> para seguir, <N> para salir");
+				
+				string otraOperacion = Console.ReadLine();
+				if(otraOperacion.ToUpper()=="N"){
+					
+					otraCuenta=false;
+					
+				}
+				
 			}
 		}
 		
-		public static void OpcionB (Banco banco){
-			
-			
-			
-		}
 	}
 }
